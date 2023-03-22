7a1b0f960dc56a59f84b6530b26b8d1654480369
unknown <anhchien1q2e@gmail.com>
2023-03-22 23:24:12 +0700

    bailuyentap5

---
+++ Cau 2/DanhSachHinh.java
@@ -0,0 +1,18 @@
+package bailuyentap5.cau2;
+
+
+public class DanhSachHinh {
+    private ArrylistHinh<Hinh>dshinh;
+    private static class ArrylistHinh<T> {
+
+        public ArrylistHinh() {
+            
+        }
+    }
+    public DanhSachHinh(){
+    }
+    public void ThemHinh(){
+    }
+    public void indanhsach(){
+    }  
+}
